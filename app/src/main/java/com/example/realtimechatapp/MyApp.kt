c14f6296e15a45e838eab2002170783b2006ddc9
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-06-30 21:08:18 +0300

    register firebase module in my app koin setup

--- app/src/main/java/com/example/realtimechatapp/MyApp.kt
+++ app/src/main/java/com/example/realtimechatapp/MyApp.kt
@@ -1,6 +1,8 @@
 package com.example.realtimechatapp
 
 import android.app.Application
+import com.example.realtimechatapp.core.firebase.firebaseModule
+import com.example.realtimechatapp.core.firebase.initializeFirebase
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -8,8 +10,12 @@ import org.koin.core.context.startKoin
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        initializeFirebase(context = this)
         startKoin {
             androidContext(this@MyApp)
+            modules(
+                firebaseModule,
+            )
         }
     }
 }
