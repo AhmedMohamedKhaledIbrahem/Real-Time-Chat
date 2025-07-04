3493f2eb86c8bbf12e480873718b0d928921286e
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-04 09:28:00 +0300

    feat(network): register internet-connection-monitor in NetworkModule

--- app/src/main/java/com/example/realtimechatapp/core/module/NetworkModule.kt
+++ app/src/main/java/com/example/realtimechatapp/core/module/NetworkModule.kt
@@ -1,2 +1,9 @@
 package com.example.realtimechatapp.core.module
 
+import com.example.internet_connection_monitor.network.InternetConnectionMonitor
+import com.example.internet_connection_monitor.network.InternetConnectionMonitorImpl
+import org.koin.dsl.module
+
+val networkModule = module {
+    single<InternetConnectionMonitor> { InternetConnectionMonitorImpl(get()) }
+}
