c72add61742937e5a511f7427e6ca039a96795fd
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-04 09:26:54 +0300

    feat(error): define authentication-related erros in data and domain layers

--- app/src/main/java/com/example/realtimechatapp/core/error/AuthDataError.kt
+++ app/src/main/java/com/example/realtimechatapp/core/error/AuthDataError.kt
@@ -1,21 +1,18 @@
 package com.example.realtimechatapp.core.error
 
-sealed interface DomainError: Error {
-    enum class Network: DomainError {
-
-        NETWORK_UNAVAILABLE,
-        TIMEOUT,
-        UNKNOWN,
-
-    }
-    enum class Auth : DomainError {
+sealed interface AuthDataError: Error {
+    enum class Network: AuthDataError {
         AUTH_FAILED,
         USER_NOT_FOUND,
         USER_ALREADY_EXISTS,
         EMAIL_NOT_VERIFIED,
         PERMISSION_DENIED,
+        NETWORK_UNAVAILABLE,
+        TIMEOUT,
+        UNKNOWN,
+
     }
-    enum class Local : DomainError {
+    enum class Local : AuthDataError {
         DATABASE_ERROR,
         READ_FAILED,
         UNKNOWN
--- app/src/main/java/com/example/realtimechatapp/core/error/AuthDomainError.kt
+++ app/src/main/java/com/example/realtimechatapp/core/error/AuthDomainError.kt
@@ -1,4 +1,20 @@
 package com.example.realtimechatapp.core.error
 
-interface AuthDomainError {
+sealed interface AuthDomainError: Error {
+    enum class Network: AuthDomainError {
+        AUTH_FAILED,
+        USER_NOT_FOUND,
+        USER_ALREADY_EXISTS,
+        EMAIL_NOT_VERIFIED,
+        PERMISSION_DENIED,
+        NETWORK_UNAVAILABLE,
+        TIMEOUT,
+        UNKNOWN,
+
+    }
+    enum class Local : AuthDomainError {
+        DATABASE_ERROR,
+        READ_FAILED,
+        UNKNOWN
+    }
 }
