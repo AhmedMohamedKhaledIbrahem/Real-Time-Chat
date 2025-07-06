41303276ef62298616c0f9d1995ac8b45c852349
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-06 12:02:27 +0300

    add NO_USER_LOGGED_IN,INVALID_SIGN_UP_PARAMS,INVALID_LOGIN_PARAMS,INVALID_EMIL error cases in enum network

--- app/src/main/java/com/example/realtimechatapp/core/error/AuthDomainError.kt
+++ app/src/main/java/com/example/realtimechatapp/core/error/AuthDomainError.kt
@@ -1,17 +1,22 @@
 package com.example.realtimechatapp.core.error
 
-sealed interface AuthDomainError: Error {
-    enum class Network: AuthDomainError {
+sealed interface AuthDomainError : Error {
+    enum class Network : AuthDomainError {
         AUTH_FAILED,
         USER_NOT_FOUND,
         USER_ALREADY_EXISTS,
+        NO_USER_LOGGED_IN,
         EMAIL_NOT_VERIFIED,
         PERMISSION_DENIED,
         NETWORK_UNAVAILABLE,
+        INVALID_SIGN_UP_PARAMS,
+        INVALID_LOGIN_PARAMS,
+        INVALID_EMAIL,
         TIMEOUT,
         UNKNOWN,
 
     }
+
     enum class Local : AuthDomainError {
         DATABASE_ERROR,
         READ_FAILED,
