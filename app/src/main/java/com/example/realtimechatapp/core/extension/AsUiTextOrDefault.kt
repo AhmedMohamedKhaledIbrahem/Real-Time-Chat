2caa5d459c27bd9e044d024b09fcb34e0fcae7bc
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-13 20:25:46 +0300

    feat: add asUiTestOrDefult extenstion to map domain errors to UiText

--- app/src/main/java/com/example/realtimechatapp/core/extension/AsUiTextOrDefault.kt
+++ app/src/main/java/com/example/realtimechatapp/core/extension/AsUiTextOrDefault.kt
@@ -1,2 +1,17 @@
 package com.example.realtimechatapp.core.extension
 
+import com.example.realtimechatapp.core.error.AuthDomainError
+import com.example.realtimechatapp.core.ui_text.UiText
+import com.example.realtimechatapp.core.utils.Result
+import com.example.realtimechatapp.core.utils.RootError
+import com.example.realtimechatapp.features.authentication.presentation.asUiText
+
+
+inline fun <reified E : RootError> Result.Error<*, E>.asUiTextOrDefault(
+    default: UiText = UiText.DynamicString(""),
+): UiText {
+    return when (val err = this.error) {
+        is AuthDomainError -> err.asUiText()
+        else -> default
+    }
+}
