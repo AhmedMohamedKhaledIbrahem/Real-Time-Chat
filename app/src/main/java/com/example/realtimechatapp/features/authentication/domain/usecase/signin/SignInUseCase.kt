7b6c63da3d3240bb42988012c1f6ae5495176639
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-13 20:58:25 +0300

    feat(authentication): add EmailValidator to validate email format in SignInUseCase

--- app/src/main/java/com/example/realtimechatapp/features/authentication/domain/usecase/signin/SignInUseCase.kt
+++ app/src/main/java/com/example/realtimechatapp/features/authentication/domain/usecase/signin/SignInUseCase.kt
@@ -1,17 +1,25 @@
 package com.example.realtimechatapp.features.authentication.domain.usecase.signin
 
+import android.util.Patterns
 import com.example.realtimechatapp.core.error.AuthDomainError
-import com.example.realtimechatapp.features.authentication.domain.repository.AuthenticationRepository
 import com.example.realtimechatapp.core.utils.Result
+import com.example.realtimechatapp.features.authentication.domain.repository.AuthenticationRepository
+import com.example.realtimechatapp.features.authentication.domain.validator.EmailValidator
 
 interface SignInUseCase {
     suspend operator fun invoke(email: String, password: String): Result<Unit, AuthDomainError>
 }
 
-class SignInUseCaseImpl(private val repository: AuthenticationRepository) : SignInUseCase {
+class SignInUseCaseImpl(
+    private val repository: AuthenticationRepository,
+    private val emailValidator: EmailValidator,
+) : SignInUseCase {
     override suspend fun invoke(email: String, password: String): Result<Unit, AuthDomainError> {
-        if ((email.isBlank() || !email.contains("@")) || password.isEmpty()) {
+        if (email.isBlank() || password.isBlank()) {
             return Result.Error(AuthDomainError.Network.INVALID_LOGIN_PARAMS)
+        }
+        if (!emailValidator.isValid(email)) {
+            return Result.Error(AuthDomainError.Network.INVALID_EMAIL)
         }
         return repository.signIn(email, password)
     }
