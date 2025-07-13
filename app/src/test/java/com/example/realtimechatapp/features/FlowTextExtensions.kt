4cc8b46dbdb6c7b83c83f0d6521959652811cb6c
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-13 21:04:59 +0300

    test: add FlowTextExtensions and MainDispatcherRule for unit testing support

--- app/src/test/java/com/example/realtimechatapp/features/FlowTextExtensions.kt
+++ app/src/test/java/com/example/realtimechatapp/features/FlowTextExtensions.kt
@@ -1,2 +1,9 @@
-package com.example.realtimechatapp.features.authentication
+package com.example.realtimechatapp.features
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+fun <T>CoroutineScope.activeTestFLow(stateFlow: StateFlow<T>) = launch {
+    stateFlow.collect {}
+}
