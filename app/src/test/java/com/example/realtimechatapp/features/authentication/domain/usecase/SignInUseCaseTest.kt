dbafa0f71c095b1ba177c59d86babd33d2293956
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-13 21:05:57 +0300

    test(authentication): add unit tests for SignInUseCase

--- app/src/test/java/com/example/realtimechatapp/features/authentication/domain/usecase/SignInUseCaseTest.kt
+++ app/src/test/java/com/example/realtimechatapp/features/authentication/domain/usecase/SignInUseCaseTest.kt
@@ -1,4 +1,81 @@
 package com.example.realtimechatapp.features.authentication.domain.usecase
 
+import com.example.realtimechatapp.core.error.AuthDomainError
+import com.example.realtimechatapp.core.utils.Result
+import com.example.realtimechatapp.features.authentication.domain.repository.AuthenticationRepository
+import com.example.realtimechatapp.features.authentication.domain.usecase.signin.SignInUseCase
+import com.example.realtimechatapp.features.authentication.domain.usecase.signin.SignInUseCaseImpl
+import com.example.realtimechatapp.features.authentication.domain.validator.EmailValidator
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
 class SignInUseCaseTest {
+    private val repository = mockk<AuthenticationRepository>()
+    private val regexEmailValidator = mockk<EmailValidator>()
+    private lateinit var useCase: SignInUseCase
+
+    @Before
+    fun setUp() {
+        useCase = SignInUseCaseImpl(repository = repository, emailValidator = regexEmailValidator)
+    }
+
+    @After
+    fun tearDown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `signIn should call repository's signIn method with correct parameters`() {
+        runBlocking {
+            val email = "test@example.com"
+            val password = "password123"
+            every { regexEmailValidator.isValid(email) } returns true
+            coEvery { repository.signIn(email, password) } returns Result.Success(Unit)
+            val result = useCase.invoke(email, password)
+            assertTrue(result is Result.Success)
+            coVerify(exactly = 1) { repository.signIn(email, password) }
+        }
+    }
+
+    @Test
+    fun `signIn should return error when email or password is blank`() {
+        runBlocking {
+            val email = ""
+            val password = ""
+            coEvery {
+                repository.signIn(email, password)
+            } returns Result.Error(AuthDomainError.Network.INVALID_LOGIN_PARAMS)
+            val result = useCase.invoke(email, password)
+            assertTrue(
+                result is Result.Error &&
+                        result.error == AuthDomainError.Network.INVALID_LOGIN_PARAMS
+            )
+            coVerify(exactly = 0) { repository.signIn(email, password) }
+        }
+    }
+    @Test
+    fun `signIn should return error when email is not contains @`() {
+        runBlocking {
+            val email = "sda23.com"
+            val password = "password123"
+            every { regexEmailValidator.isValid(email) } returns false
+            coEvery {
+                repository.signIn(email, password)
+            } returns Result.Error(AuthDomainError.Network.INVALID_EMAIL)
+            val result = useCase.invoke(email, password)
+            assertTrue(
+                result is Result.Error &&
+                        result.error == AuthDomainError.Network.INVALID_EMAIL
+            )
+            coVerify(exactly = 0) { repository.signIn(email, password) }
+        }
+    }
 }
