3d5e206e91fc9f5904dbd71231f69203d6e0a208
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-13 20:33:33 +0300

    feat: add performUseCaseOperation extension for viewModel to handle the usecase execution with success and error callbacks

--- app/src/main/java/com/example/realtimechatapp/core/extension/performUseCaseOperation.kt
+++ app/src/main/java/com/example/realtimechatapp/core/extension/performUseCaseOperation.kt
@@ -1,2 +1,33 @@
 package com.example.realtimechatapp.core.extension
 
+import androidx.lifecycle.ViewModel
+import com.example.realtimechatapp.core.utils.Result
+import com.example.realtimechatapp.core.utils.RootError
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+inline fun <D, reified E : RootError> ViewModel.performUseCaseOperation(
+    crossinline useCase: suspend () -> Result<D, E>,
+    crossinline onSuccess: suspend (D) -> Unit,
+    crossinline onError: suspend (Result.Error<*, E>) -> Unit,
+    scope: CoroutineScope
+) {
+    scope.launch {
+        val result = useCase()
+        when (result) {
+            is Result.Success -> onSuccess(result.data)
+            is Result.Error -> onError(result)
+        }
+    }
+}
+
+inline fun <D, reified E : RootError> ViewModel.performUseCaseOperation(
+    crossinline useCase: () -> Result<D, E>,
+    crossinline onSuccess: (D) -> Unit,
+    crossinline onError: (Result.Error<*, E>) -> Unit,
+) {
+    when (val result = useCase()) {
+        is Result.Success -> onSuccess(result.data)
+        is Result.Error -> onError(result)
+    }
+}
