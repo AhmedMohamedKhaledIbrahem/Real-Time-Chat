89de769576e1274c550682b7f06697bf833403b0
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-17 11:44:40 +0300

    feat(authentication): add import path SignInModel

--- app/src/main/java/com/example/realtimechatapp/features/authentication/data/mapper/UserMapper.kt
+++ app/src/main/java/com/example/realtimechatapp/features/authentication/data/mapper/UserMapper.kt
@@ -1,6 +1,7 @@
 package com.example.realtimechatapp.features.authentication.data.mapper
 
 import com.example.realtimechatapp.core.database.data.entity.user.UserEntity
+import com.example.realtimechatapp.features.authentication.data.model.SignUpModel
 import com.example.realtimechatapp.features.authentication.data.model.UserModel
 
 fun UserModel.toUserEntity(uid: String): UserEntity = UserEntity(
@@ -9,4 +10,10 @@ fun UserModel.toUserEntity(uid: String): UserEntity = UserEntity(
     email = email,
     image = imageUrl,
     phoneNumber = phone,
+)
+fun UserModel.toSignUpModel(): SignUpModel = SignUpModel(
+    name = name,
+    email = email,
+    phoneNumber = phone,
+    password = ""
 )
