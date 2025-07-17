5f7f7f010cf6f22f460bc87b3067b2180d3a4a69
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-07-17 11:53:40 +0300

    feat(authentication): add import path flow and flow.take

--- app/src/test/java/com/example/realtimechatapp/features/FlowTextExtensions.kt
+++ app/src/test/java/com/example/realtimechatapp/features/FlowTextExtensions.kt
@@ -1,10 +1,12 @@
 package com.example.realtimechatapp.features
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
 
-fun <T>CoroutineScope.activeTestFLow(stateFlow: StateFlow<T>) = launch {
+fun <T> CoroutineScope.activeTestFLow(stateFlow: StateFlow<T>) = launch {
     stateFlow.collect {}
 }
 
