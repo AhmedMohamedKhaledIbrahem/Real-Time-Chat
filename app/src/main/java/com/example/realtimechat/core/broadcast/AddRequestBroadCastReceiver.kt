8a1b3655fec87f6521358955ac0f676a90fc5cc1
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-08-01 09:25:10 +0300

    feat(core): add AddRequestBroadCastReceiver

--- app/src/main/java/com/example/realtimechat/core/broadcast/AddRequestBroadCastReceiver.kt
+++ app/src/main/java/com/example/realtimechat/core/broadcast/AddRequestBroadCastReceiver.kt
@@ -1,2 +1,19 @@
 package com.example.realtimechat.core.broadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.realtimechat.R
+
+class AddRequestBroadCastReceiver : BroadcastReceiver(){
+    override fun onReceive(p0: Context, p1: Intent) {
+        when(p1.action){
+            p0.getString(R.string.Accept) -> {
+
+            }
+            p0.getString(R.string.Reject) -> {
+
+            }
+        }
+    }
+}
