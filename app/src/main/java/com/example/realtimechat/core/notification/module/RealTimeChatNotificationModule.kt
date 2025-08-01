bffdda2446ec54c9e24309f4d1c194d7d72fb29a
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-08-01 09:11:37 +0300

    feat(core): add Notification and configure DI Module

--- app/src/main/java/com/example/realtimechat/core/notification/module/RealTimeChatNotificationModule.kt
+++ app/src/main/java/com/example/realtimechat/core/notification/module/RealTimeChatNotificationModule.kt
@@ -1,2 +1,9 @@
 package com.example.realtimechat.core.notification.module
 
+import com.example.realtimechat.core.notification.RealTimeChatNotification
+import com.example.realtimechat.core.notification.RealTimeChatNotificationImpl
+import org.koin.dsl.module
+
+val realTimeChatNotificationModule = module {
+    single<RealTimeChatNotification> { RealTimeChatNotificationImpl(get()) }
+}
