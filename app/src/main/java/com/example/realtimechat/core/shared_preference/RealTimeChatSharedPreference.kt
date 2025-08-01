05671a5f832dde180d5bbe3d97c3c0ac9314e5b4
Ahmed ALFeky <ahmed.m.khaled.2020@gmail.com>
2025-08-01 09:10:57 +0300

    feat(core): add shared preference and configure DI module

--- app/src/main/java/com/example/realtimechat/core/shared_preference/RealTimeChatSharedPreference.kt
+++ app/src/main/java/com/example/realtimechat/core/shared_preference/RealTimeChatSharedPreference.kt
@@ -1,2 +1,49 @@
 package com.example.realtimechat.core.shared_preference
 
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import com.example.realtimechat.core.logger.Logger
+
+interface RealTimeChatSharedPreference {
+    fun saveToken(token: String)
+    fun getToken(): String?
+    fun clearToken()
+}
+
+class RealTimeChatSharedPreferenceImpl(
+    private val sharedPreference: SharedPreferences,
+    logger: Logger
+) : RealTimeChatSharedPreference, Logger by logger {
+    override fun saveToken(token: String) {
+        try {
+            sharedPreference.edit { putString(TOKEN, token) }
+        } catch (e: Exception) {
+            e(e, ERROR_SAVING_TOKEN)
+        }
+    }
+
+    override fun getToken(): String? {
+        return try {
+            sharedPreference.getString(TOKEN, null)
+        } catch (e: Exception) {
+            e(e, ERROR_GETTING_TOKEN)
+            null
+        }
+    }
+
+    override fun clearToken() {
+        try {
+            sharedPreference.edit { remove(TOKEN) }
+        } catch (e: Exception) {
+            e(e, ERROR_CLEARING_TOKEN)
+        }
+    }
+
+    companion object {
+        const val TOKEN = "token"
+        const val ERROR_SAVING_TOKEN = "Error saving token"
+        const val ERROR_GETTING_TOKEN = "Error getting token"
+        const val ERROR_CLEARING_TOKEN = "Error clearing token"
+    }
+
+}
